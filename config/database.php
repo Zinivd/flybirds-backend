ae972774c135e750891423fd15566d2522a48592
Zinivd Tech <zinivd.cloud@gmail.com>
2026-09-04 17:06:04 +0530

    data

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_PERSISTENT => true, // Reuses open database handles across requests
+                PDO::ATTR_PERSISTENT => false, // Reuses open database handles across requests
             ]) : [],
         ],
 
